42d78fecf04dc2c46a810c28285783239ec9a815
unknown <carter.adrian@gmail.com>
2023-01-02 17:36:28 -0500

    add server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server available at localhost${PORT}`);
+});
