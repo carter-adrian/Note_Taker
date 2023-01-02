47374bffccd804f3b29b78fa27d0900bce5da6b3
unknown <carter.adrian@gmail.com>
2023-01-02 17:22:08 -0500

    html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// dependencies
+const path = require('path');
+
+//routing
+module.exports = (app) => {
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
+    
