d8539bda3769c92b0a678fee403ebf2f28473fd7
unknown <carter.adrian@gmail.com>
2023-01-02 18:22:31 -0500

    testing and tweaking

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,19 @@
+
 // dependencies
 const path = require('path');
 
-//routing
+
+// routing
 module.exports = (app) => {
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
+  // creating routes
+  // GET /notes should return the notes.html file.
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
+  // GET * should return the index.html file.
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  })
 };
-    
